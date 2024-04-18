225b0e6d9c4c6a6cdadd50d8fcc87bbc31c8273c
IsaqjonovII <ilhomjonisoqjonov2@gmail.com>
2024-04-18 16:36:33 +0500

    working on it

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import jsConfigPaths from "vite-jsconfig-paths"
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), jsConfigPaths()],
 })
