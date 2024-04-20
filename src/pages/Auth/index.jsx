3c1ab3bad3df8f006701eeea4f015c81e25e33a3
IsaqjonovII <ilhomjonisoqjonov2@gmail.com>
2024-04-20 15:53:59 +0500

    working

--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -36,7 +36,7 @@ const Login = () => {
         >
           <Form.Item
             label="Email"
-            name="Email"
+            name="email"
             rules={[
               {
                 required: true,
